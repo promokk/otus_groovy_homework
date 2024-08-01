e6eb9044d94a44f0b121c6c562d57f4917d6c3f6
promokk <andrey1803vorobyv@yandex.ru>
2024-08-02 01:25:20 +0300

    Create project otus-groovy

---
+++ otus-groovy/hw01-gradle/src/main/groovy/org/example/otus/HelloOtus.groovy
@@ -0,0 +1,11 @@
+package org.example.otus
+
+import com.google.common.base.Strings
+
+static void main(String[] args) {
+
+    def str = ""
+    if (Strings.isNullOrEmpty(str)) {
+        println "Hello Otus!"
+    }
+}
---
+++ otus-groovy/src/main/groovy/org/example/Main.groovy
@@ -0,0 +1,6 @@
+package org.example
+
+static void main(String[] args) {
+
+    println "Hello World!"
+}
