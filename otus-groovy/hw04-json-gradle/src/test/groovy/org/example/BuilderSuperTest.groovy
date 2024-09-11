bc98e16ea32b50c445b94ddbf89d850e6fc84c25
promokk <andrey1803vorobyv@yandex.ru>
2024-09-12 01:32:20 +0300

    hw04-json-gradle: - поправил тесты

--- otus-groovy/hw04-json-gradle/src/test/groovy/org/example/BuilderSuperTest.groovy
+++ otus-groovy/hw04-json-gradle/src/test/groovy/org/example/BuilderSuperTest.groovy
@@ -13,7 +13,7 @@ class BuilderSuperTest {
         def testHtml = "<html>\n" +
                 "  <div>\n" +
                 "    <div id='employee'>\n" +
-                "      <p>РџСѓРїРєРёРЅ РњРѕСЂРєРІР° РЎРІРµРєР»РѕРІРёС‡</p>\n" +
+                "      <p>Пупкин Морква Свеклович</p>\n" +
                 "      <p>22</p>\n" +
                 "      <p>322-223</p>\n" +
                 "      <ul id='powers'>\n" +
@@ -32,7 +32,7 @@ class BuilderSuperTest {
     void testBuilderXml() {
         def testXml = "<xml>\n" +
                 "  <employee>\n" +
-                "    <name>РџСѓРїРєРёРЅ РњРѕСЂРєРІР° РЎРІРµРєР»РѕРІРёС‡</name>\n" +
+                "    <name>Пупкин Морква Свеклович</name>\n" +
                 "    <age>22</age>\n" +
                 "    <secretIdentity>322-223</secretIdentity>\n" +
                 "    <powers>\n" +
